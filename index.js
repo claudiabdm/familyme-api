559ecf3e00a6091e8bcf156463f624b2cfa3fe53
Claudia <claudiabdm@hotmail.es>
2020-04-09 00:37:40 +0200

    fix dependencies

--- index.js
+++ index.js
@@ -28,8 +28,13 @@ const onDBReady = (err) => {
   //   }
   //   console.log('Koa server listening in port 3000');
   // });
-
-  app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000, function (err) {
+    if (err) {
+      console.error('Error listening in port 3000', err);
+      process.exit(1);
+    }
+    console.log('Koa server listening in port 3000');
+  });
 }
 
 mongoose.connect(mongoUri, {
